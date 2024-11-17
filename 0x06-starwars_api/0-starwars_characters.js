3dfbd63e0295d765be1aff7f0210aff8e6362456
stevovenom <stephenokoth2002@gmail.com>
2024-11-17 23:13:11 +0300

    update on the star wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,63 @@
 #!/usr/bin/node
-const util = require('util');
-const request = util.promisify(require('request'));
-const filmID = process.argv[2];
 
-async function starwarsCharacters (filmId) {
-  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-  let response = await (await request(endpoint)).body;
-  response = JSON.parse(response);
-  const characters = response.characters;
+const request = require('request');
 
-  for (let i = 0; i < characters.length; i++) {
-    const urlCharacter = characters[i];
-    let character = await (await request(urlCharacter)).body;
-    character = JSON.parse(character);
-    console.log(character.name);
-  }
+// Get the Movie ID from the command line arguments
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
 }
 
-starwarsCharacters(filmID);
+// Base URL for the Star Wars API
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+// Fetch the movie details
+request(apiUrl, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Error: Received status code ${response.statusCode}`);
+    return;
+  }
+
+  // Parse the movie data
+  const movieData = JSON.parse(body);
+  const characters = movieData.characters;
+
+  // Function to fetch a character's name
+  const fetchCharacter = (url, callback) => {
+    request(url, (err, response, body) => {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      if (response.statusCode !== 200) {
+        callback(`Error: Received status code ${response.statusCode}`, null);
+        return;
+      }
+      const characterData = JSON.parse(body);
+      callback(null, characterData.name);
+    });
+  };
+
+  // Fetch all characters in sequence
+  const printCharacters = (index) => {
+    if (index >= characters.length) {
+      return;
+    }
+    fetchCharacter(characters[index], (err, name) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(name);
+      printCharacters(index + 1);
+    });
+  };
+
+  printCharacters(0);
+});
